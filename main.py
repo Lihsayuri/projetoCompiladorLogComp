4f00605087c09802d05defd8cd97136b71ceaf6e
Lihsayuri <liviasm1@al.insper.edu.br>
2023-02-27 17:22:48 -0300

    Roteiro2 feito!

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import sys
 
+class PrePro:
+    def filter(source):
+        while "#" in source:
+            start = source.find("#")
+            if source[start:].find("\n") == -1:
+                end = len(source)
+            else:
+                end = source.find("\n", start) + 2 # índice start para a função find, garante que a busca pelo fim comece a partir do início 
+            comment = source[start:end]
+            source = source.replace(comment, "")
+        return source
 
 class Token:
     def __init__(self, type, value):
@@ -35,35 +46,61 @@ class Tokenizer:
                 self.next = Token("MINUS", 0)
                 self.position += 1
                 return 
+            elif self.source[self.position] == '*':
+                self.next = Token("MULT", 0)
+                self.position += 1
+                return
+            elif self.source[self.position] == '/':
+                self.next = Token("DIV", 0)
+                self.position += 1
+                return
             elif self.source[self.position] == " ":
                 self.position += 1
             else:
                 sys.stderr.write("Você digitou um caracter inválido")
                 sys.exit(1)
-        else:
-            self.next = Token("EOF", 0)
-            return
+        
+        self.next = Token("EOF", 0)
+        return
 
 class Parser:
     tokenizer = None
 
     def parseExpression(tokenizer):
+        resultado = Parser.parseTerm(tokenizer)
+        if tokenizer.next.type == "PLUS" or tokenizer.next.type == "MINUS" or tokenizer.next.type == "EOF":
+            while tokenizer.next.type == "PLUS" or tokenizer.next.type == "MINUS":
+                if tokenizer.next.type == "PLUS":
+                    resultado +=  Parser.parseTerm(tokenizer)
+                if tokenizer.next.type == "MINUS":            
+                    resultado -=  Parser.parseTerm(tokenizer)
+            return resultado
+
+        if tokenizer.next.type == "INT":
+            sys.stderr.write("Erro de sintaxe")
+            sys.exit(1)
+
+    def parseTerm(tokenizer):
         resultado = 0
+        tokenizer.selectNext()
         if tokenizer.next.type == "INT":
             resultado = int(tokenizer.next.value)
             tokenizer.selectNext()
-            while tokenizer.next.type == "PLUS" or tokenizer.next.type == "MINUS":
-                if tokenizer.next.type == "PLUS":
+            while tokenizer.next.type == "MULT" or tokenizer.next.type == "DIV":
+                if tokenizer.next.type == "MULT":
                     tokenizer.selectNext()
                     if tokenizer.next.type == "INT":
-                        resultado += int(tokenizer.next.value)
+                        resultado *= int(tokenizer.next.value)
                     else:
                         sys.stderr.write("Erro de sintaxe: operador depois de operador")
                         sys.exit(1)
-                if tokenizer.next.type == "MINUS":            
+                if tokenizer.next.type == "DIV":            
                     tokenizer.selectNext()
                     if tokenizer.next.type == "INT":
-                        resultado -= int(tokenizer.next.value)
+                        if int(tokenizer.next.value) == 0:
+                            sys.stderr.write("Erro de sintaxe: divisão por zero")
+                            sys.exit(1)
+                        resultado //= int(tokenizer.next.value)
                     else:
                         sys.stderr.write("Erro de sintaxe: operador depois de operador")
                         sys.exit(1)
@@ -75,11 +112,10 @@ class Parser:
             sys.stderr.write("Erro de sintaxe: não pode começar com operador")
             sys.exit(1)
 
-
  
     def run(code):
+        code = PrePro.filter(code)
         Parser.tokenizer = Tokenizer(code, 0)
-        Parser.tokenizer.selectNext()
         resultado = Parser.parseExpression(Parser.tokenizer)
 
         if Parser.tokenizer.position == len(Parser.tokenizer.source) and Parser.tokenizer.next.type == "EOF":
